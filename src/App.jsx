6e28ed4fcd98503fdf9d5f9e1651ede14124f948
MH--Hassan02 <haseebhassan1155@gmail.com>
2025-09-18 17:24:19 +0500

    corrected import route

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
-import AppRoutes from "./routes/appRoutes";
 import Loader from "./components/loader/Loader";
+import AppRoutes from "./pages/routes/AppRoutes";
 
 function App() {
   return (
